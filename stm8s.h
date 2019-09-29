1727d2bee28d4190896f315955d00d0ca7e6d900
github150620 <597540267@qq.com>
2019-09-29 21:35:52 +0800

    Update stm8s.h

--- stm8s.h
+++ stm8s.h
@@ -35,4 +35,11 @@
 #define PF_CR1 *(unsigned char*)0x501C
 #define PF_CR2 *(unsigned char*)0x501D
 
+/* FLASH */
+#define FLASH_CR1   *(unsigned char*)0x505A
+#define FLASH_CR2   *(unsigned char*)0x505B
+#define FLASH_NCR2  *(unsigned char*)0x505C
+#define FLASH_FPR   *(unsigned char*)0x505D
+#define FLASH_NFPR  *(unsigned char*)0x505E
+#define FLASH_IAPSR *(unsigned char*)0x505F
 
